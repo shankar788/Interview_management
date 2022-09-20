20ce6b3f868c452fd82d58f5f18709e7ce37d6c9
shankarwk <shankar@webkorps.com>
2022-09-20 12:39:54 -0400

    m

--- db/migrate/20220805193229_droptable.rb
+++
@@ -1,5 +0,0 @@
-class Droptable < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :drives
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_08_23_203301) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "mydates", force: :cascade do |t|
     t.string "date"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ten"
